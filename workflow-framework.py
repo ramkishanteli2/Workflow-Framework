a774e0d72df599db2694e9d14cf1bd0d6b3bfe74
= <=>
2022-03-08 15:12:03 +0530

    first mile stone done

--- workflow-framework.py
+++ workflow-framework.py
@@ -5,13 +5,17 @@ import logging
 pathList = []
 
 
-def timeFunction():
+def path2String(pathList):
+    return '.'.join(pathList)
+
+
+def timeFunction(func_name, exe_time):
     pass
 
 
 def generateLogFile(data, key):
     pathList.append(key)
-    logging.info(key + " Entry")
+    logging.warning(path2String(pathList) + " Entry")
 
     if(data['Type'] == 'Task'):
 
@@ -19,23 +23,33 @@ def generateLogFile(data, key):
 
             func_name = data['Inputs']['FunctionInput']
             exe_time = data['Inputs']['ExecutionTime']
-
+            logging.warning(path2String(pathList) + " Executing " +
+                            "TimeFunction " + "(" + func_name + ", " + exe_time + ")")
             timeFunction(func_name, exe_time)
-            path.pop()
-    logging.info(convertPath2String(path) + " Exit")
-    path.pop()
+
+    elif (data['Type'] == 'Flow'):
+
+        if(data['Execution'] == 'Sequential'):
+            for activity in data['Activities']:
+                generateLogFile(data['Activities'][activity], activity)
+
+        elif(data['Execution'] == 'Concurrent'):
+            # INCOMPLETE
+            pass
+    logging.warning(path2String(pathList) + " Exit")
+    pathList.pop()
 
 
 # Opening yaml file
-yaml_file = open('Milestone1_Example.yaml', mode='r')
+yaml_file = open('DataSet\Milestone1/Milestone1A.yaml', mode='r')
 
 # Reading data from yaml file
 data = yaml.load(yaml_file, Loader=Loader)
 
 # Generating log files using data dictionary obtained from yaml file
-basicConfig(filename="logfile.txt", format="%(asctime)s.%(msecs)06d;%(message)s",
-            datefmt='%Y-%m-%d %H:%M:%S', filemode='w')
+logging.basicConfig(filename="logfile.txt", format="%(asctime)s.%(msecs)06d;%(message)s",
+                    datefmt='%Y-%m-%d %H:%M:%S', filemode='w')
 
+print(data)
 for workflow in data:
-    pass
-    # incomplete
+    generateLogFile(data[workflow], workflow)
